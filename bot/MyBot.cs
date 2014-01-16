7e293b0db11525ccd9ef4fef2c781471ca5fa16d
ZJvandeWeg <zegerjan@gmail.com>
2014-01-16 15:19:14 +0100

    wp_ first try at q learning

--- bot/MyBot.cs
+++ bot/MyBot.cs
@@ -4,9 +4,29 @@ using System.Collections.Generic;
 namespace Ants {
 
 	class MyBot : Bot {
+		int[,] R;
+		double [,] Q;
+		const double Alpha = 0.6; //
+		const double Gamma = 0.6; //
+
+		const int WaterReward 	= -10;
+		const int FoodReward 	= 50;
 
 		// DoTurn is run once per turn
 		public override void DoTurn (IGameState state) {
+			if(R == null) {
+				R = new int[state.Width, state.Height];
+				Q = new double[state.Width, state.Height];
+			}
+
+			for (int x = 0; x < state.Width; x++) {
+				for (int y = 0; y < state.Height; y++) {
+					if (!state.IsPassable(x, y))
+						R[x,y] = WaterReward;
+					else if (state[x,y] == Tile.Food)
+						R[x,y] = FoodReward; 
+				}	
+			}
 
 			// loop through all my ants and try to give them orders
 			foreach (Ant ant in state.MyAnts) {
@@ -25,18 +45,19 @@ namespace Ants {
 						break;
 					}
 				}
-				
 				// check if we have time left to calculate more orders
 				if (state.TimeRemaining < 10) break;
 			}
-			
+
+			double q = Q[state, action];
+			int r = R[state, action];
+			double maxQ = maxQ(action);
+			Q[state, action] = (1.0 - Alpha) * q + Alpha * (r + Gamma * maxQ);
 		}
 		
 		
 		public static void Main (string[] args) {
 			new Ants().PlayGame(new MyBot());
 		}
-
 	}
-	
 }
